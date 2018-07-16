09a2c881e0d0f4c20269b757f0fa376b4b71a9c4
sivanWu <1984430988@qq.com>
2018-07-16 11:16:38 +0800

    init project

---
+++ WXScrollLabel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WXScrollLabel
+//
+//  Created by HFY on 2018/7/16.
+//  Copyright © 2018年 wuxi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
