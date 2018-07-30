88ee3a66553ae64712508c957cea0b76a1f47273
sivanWu <1984430988@qq.com>
2018-07-30 16:46:19 +0800

    add framework and support carthage

---
+++ CyclicLabel/CyclicLabel.h
@@ -0,0 +1,19 @@
+//
+//  CyclicLabel.h
+//  CyclicLabel
+//
+//  Created by HFY on 2018/7/30.
+//  Copyright © 2018年 wuxi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CyclicLabel.
+FOUNDATION_EXPORT double CyclicLabelVersionNumber;
+
+//! Project version string for CyclicLabel.
+FOUNDATION_EXPORT const unsigned char CyclicLabelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CyclicLabel/PublicHeader.h>
+
+#import <CyclicLabel/WXCyclicScrollLabel.h>
